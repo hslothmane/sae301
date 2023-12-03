68b95ba7a0357682e8d0a0184bf70e7f2ce0c3f5
hslothmane <shadidisalah@gmail.com>
2023-12-03 01:01:43 +0000

    four

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function index(): Response
+    {
+        return $this->render('connexion/index.html.twig');
+    }
+}
+
